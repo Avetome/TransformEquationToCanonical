3ba16d8b328be47535535874abd9cc8d04cb269a
Avetome <avetome@gmail.com>
2019-02-17 14:25:10 +0300

    Handling user input mode and file mode

--- EquationTransformer/Program.cs
+++ EquationTransformer/Program.cs
@@ -1,29 +1,111 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace EquationTransformer
 {
     class MainClass
     {
+        private const ConsoleColor NormalColor = ConsoleColor.White;
+        private const ConsoleColor MessagesColor = ConsoleColor.Gray;
+        private const ConsoleColor ResultColor = ConsoleColor.White;
+        private const ConsoleColor ErrorColor = ConsoleColor.Red;
+
         public static void Main(string[] args)
         {
-            /*var testString = "10 + 20 - 30.123 - -1";
-            var t = new Tokenizer(new StringReader(testString));
-
-            while(t.Token != Token.EOF)
+            if (args.Length == 0)
             {
-                Console.WriteLine($"{t.Number} {t.Token}");
-                t.NextToken();
-            }*/
+                UserInputMode();
+            }
+            else
+            {
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine($"File {args[0]} not found");
+                }
 
-            var testString = "2x";
-            var parser = new Parser(testString);
+                FileMode(args[0]);
+            }
+        }
 
-            var summand = parser.GetSummand().First();
+        private static void UserInputMode()
+        {
+            Console.Clear();
 
-            Console.WriteLine($"{summand.Multiplier} {summand.Variables.FirstOrDefault().Key} ^ {summand.Variables.FirstOrDefault().Value}");
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
 
-            Console.ReadLine();
+            Console.Title = "Transform equation To canonical form";
+
+            Console.ForegroundColor = MessagesColor;
+            Console.WriteLine("***** Press CTRL+C to exit the program *****");
+            Console.ForegroundColor = NormalColor;
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = MessagesColor;
+                Console.Write("Enter the equation: ");
+                Console.ForegroundColor = NormalColor;
+
+                string equation = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(equation))
+                {
+                    continue;
+                }
+
+                EquationTrasformer equationTrasformer = new EquationTrasformer();
+                try
+                {
+                    string result = equationTrasformer.Transform(equation);
+
+                    Console.ForegroundColor = ResultColor;
+                    Console.Write("Equation canonical form: ");
+
+                    Console.Write(result);
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = NormalColor;
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ErrorColor;
+                    Console.WriteLine("Enter a valid equation!");
+                    Console.ForegroundColor = NormalColor;
+                }
+            }
+        }
+
+        private static void FileMode(string filename)
+        {
+            string line, result;
+
+            using (var inputFile = new StreamReader(filename))
+            using (var outputFile = new StreamWriter(filename + ".out"))
+            {
+                Console.WriteLine("Processing file...");
+
+                while ((line = inputFile.ReadLine()) != null)
+                {
+                    EquationTrasformer equationTrasformer = new EquationTrasformer();
+                    result = equationTrasformer.Transform(line);
+
+                    outputFile.WriteLine(result);
+                }
+                inputFile.Close();
+                outputFile.Flush();
+                outputFile.Close();
+
+                Console.WriteLine("File processing finished");
+            }
+        }
+
+
+        private static void CancelHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("Exit application");
+
+            Environment.Exit(0);
         }
     }
 }
