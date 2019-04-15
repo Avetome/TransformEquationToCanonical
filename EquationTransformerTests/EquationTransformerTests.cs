07da6b708cb0205a5d90daa2f65e04e44d7c62ed
Alexey Nikiforov <alenikiforov@ozon.ru>
2019-04-15 18:11:18 +0300

    Test data simplification example

--- EquationTransformerTests/EquationTransformerTests.cs
+++ EquationTransformerTests/EquationTransformerTests.cs
@@ -35,57 +35,28 @@ namespace EquationTransformerTests
 
         public static IEnumerable<object[]> GetEquations()
         {
-            yield return new object[] {
-                "3x5y = 4y7z",
-                "15xy - 28yz = 0;" };
+            var examples = GetEquationsData();
 
-            yield return new object[] {
-                "3x5y = -4y7z",
-                "15xy + 28yz = 0;" };
+            foreach(var example in examples)
+            {
+                yield return new object[] { example.equation, example.normalEquation };
+            }
+        }
 
-            yield return new object[] {
-                "3x5y = 4y7x",
-                "-13xy = 0;" };
-
-            yield return new object[] {
-                "x^2 + 3.5xy + y = y^2 - xy + y",
-                "x^2 - y^2 + 4.5xy = 0;" };
-
-            yield return new object[] {
-                "x = y",
-                "x - y = 0;" };
-
-            yield return new object[] {
-                "x - (y^2 - x) = 0",
-                "-y^2 + 2x = 0;" };
-
-            yield return new object[] {
-                "x - (0 - (0 - x)) = 0",
-                "0 = 0;" };
-
-            yield return new object[] {
-                "x2 = 3X",
-                "-x = 0;" };
-
-            yield return new object[] {
-                "-7 + 4x^2 + xy - x^4 + z = y - 2xy + w^2",
-                "-x^4 + 4x^2 - w^2 + 3xy + z - y - 7 = 0;" };
-
-            yield return new object[] {
-                "2 - 1 = x",
-                "-x + 1 = 0;" };
-
-            yield return new object[] {
-                "x^-1 = 5",
-                "x^-1 - 5 = 0;" };
-
-            yield return new object[] {
-                "x = y",
-                "x - y = 0;" };
-
-            yield return new object[] {
-                "x + y = 1",
-                "x + y - 1 = 0;" };
+        public static IEnumerable<(string equation, string normalEquation)> GetEquationsData()
+        {
+            yield return ("3x5y = 4y7z", "15xy - 28yz = 0;");
+            yield return ("3x5y = -4y7z", "15xy + 28yz = 0;");
+            yield return ("3x5y = 4y7x", "-13xy = 0;");
+            yield return ("x^2 + 3.5xy + y = y^2 - xy + y", "x^2 - y^2 + 4.5xy = 0;");
+            yield return ("x = y", "x - y = 0;");
+            yield return ("x - (y^2 - x) = 0", "-y^2 + 2x = 0;");
+            yield return ("x - (0 - (0 - x)) = 0", "0 = 0;");
+            yield return ("x2 = 3X", "-x = 0;");
+            yield return ("-7 + 4x^2 + xy - x^4 + z = y - 2xy + w^2", "-x^4 + 4x^2 - w^2 + 3xy + z - y - 7 = 0;");
+            yield return ("2 - 1 = x", "-x + 1 = 0;");
+            yield return ("x^-1 = 5", "x^-1 - 5 = 0;");
+            yield return ("x + y = 1", "x + y - 1 = 0;");
         }
 
         public static IEnumerable<object[]> GetInvalidEquations()
